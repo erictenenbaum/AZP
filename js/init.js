1dbaabd635b451e3d4879d3c453f99bc233ac622
erictenenbaum <erictenenbaum10@gmail.com>
2017-12-15 13:21:50 -0700

    added more features and fixed the logo issue

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.collapsible').collapsible();
+    $(".dropdown-button").dropdown();
+     $('.scrollspy').scrollSpy();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
